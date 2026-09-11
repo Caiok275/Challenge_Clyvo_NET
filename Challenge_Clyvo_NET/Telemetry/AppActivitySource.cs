43466754687b2b0b6758d51fb09013c37c4ca1f9
Caio Tayra <caio.kenzo.t@gmail.com>
2026-09-11 16:58:30 -0300

    Update AppActivitySource.cs

--- Challenge_Clyvo_NET/Telemetry/AppActivitySource.cs
+++ Challenge_Clyvo_NET/Telemetry/AppActivitySource.cs
@@ -2,16 +2,7 @@
 
 namespace Challenge_Clyvo_NET.Telemetry
 {
-    /// <summary>
-    /// Fonte de Activities (spans) customizada da aplicação. As instrumentações
-    /// automáticas (ASP.NET Core, EF Core, HttpClient) já cobrem a maior parte
-    /// do rastreamento entre camadas; esta fonte existe para criar spans manuais
-    /// em pontos de negócio específicos que valham a pena destacar em um trace
-    /// (ex.: uma regra de validação cara, uma orquestração entre várias
-    /// entidades, etc.), usando: 
-    ///   using var activity = AppActivitySource.Instance.StartActivity("NomeDaOperacao");
-    ///   activity?.SetTag("chave", valor);
-    /// </summary>
+
     public static class AppActivitySource
     {
         public const string Name = "Challenge_Clyvo_NET";
